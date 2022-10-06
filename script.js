d045251ec0a18262764ea2b41ef745eebc6fa581
CarpetFloor <dylan.bartness@gmail.com>
2022-10-06 15:01:28 -0500

    backtracking works, but it now generates a maze

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let c = document.getElementById("canvas");
 let r = c.getContext("2d");
+// c.width = Math.floor(window.innerWidth * 0.95);
+// c.height = Math.floor(window.innerHeight * 0.95);
 c.width = 500;
 c.height = c.width;
 let w = c.width;
 let h = c.height;
 let i = 0;
 let max = 500;
+let showBackground = false;
+// this was mistankenly discovered but
+let mazeMode = true;
+if(mazeMode)
+    showBackground = false;
 
 let tile = {
-    size: w / 25,
+    size: w / 50,
     data: [],
     generateData: () => {
         let repeat = w / tile.size;
@@ -28,8 +35,8 @@ tile.generateData();
 
 let path = [];
 let pos = {
-    x: 1.234,
-    y: 4.321
+    x: -1,
+    y: -1
 };
 
 function generatePath() {
@@ -39,17 +46,20 @@ function generatePath() {
 
     path.push(pos);
     
-    renderBackground();
     iteration();
 }
 generatePath();
 
 function iteration() {
-    if(i < max) {
+    if(path.length < tile.data.length * tile.data[0].length) {
         console.log(i);
         ++i;
 
-        let possible = getPossibleMoves(pos.x, pos.y);
+        // to not ignore any moves, because for backtracking only,
+        // initialize as a position that is not valid;
+        let ignoreMove = [-1, -1];
+
+        let possible = getPossibleMoves(pos.x, pos.y, ignoreMove);
 
         if(possible != false) {
             let newMove = Math.floor(Math.random() * possible.length);
@@ -64,14 +74,33 @@ function iteration() {
             // remove new position from available positions
             tile.data[pos.x][pos.y] = 1;
 
-            renderPath(i == 0);
-
-            window.setTimeout(iteration, 50);
+            renderPath();
         }
+        // backtracking
+        else {
+            console.log("backtracking");
+            // when backtracking ignore tile in before
+            ignoreMove = [pos.x, pos.y];
+
+            // remove last movement of path
+            path.pop();
+
+            // make removed part of path availible as a move
+            tile.data[pos.x][pos.y] = 1;
+
+            // update position
+            pos.x = path[path.length - 1][0];
+            pos.y = path[path.length - 1][1];
+        }
+
+        if(mazeMode)
+            window.setTimeout(iteration, 0);
+        else
+            window.setTimeout(iteration, 50);
     }
 }
 
-function getPossibleMoves(x, y) {
+function getPossibleMoves(x, y, ignoreMove) {
     let left = true;
     let right = true;
     let up = true;
@@ -81,28 +110,28 @@ function getPossibleMoves(x, y) {
     if(y == 0)
         up = false;
     // already moved into case
-    else if(tile.data[x][y - 1] == 1)
+    else if(tile.data[x][y - 1] == 1 || tile.data[x][y - 1] == ignoreMove[1])
         up = false
     
     // edge case
     if(y >= tile.data[0].length - 1)
         down = false;
     // already moved into case
-    else if(tile.data[x][y + 1] == 1)
+    else if(tile.data[x][y + 1] == 1 || tile.data[x][y + 1] == ignoreMove[1])
         down = false
     
     // edge case
     if(x == 0)
         left = false;
     // already moved into case
-    else if(tile.data[x - 1][y] == 1)
+    else if(tile.data[x - 1][y] == 1 || tile.data[x - 1][y] == ignoreMove[0])
         left = false;
     
     // edge case
     if(x >= tile.data.length - 1)
         right = false;
     // already moved into case
-    else if(tile.data[x + 1][y] == 1)
+    else if(tile.data[x + 1][y] == 1 || tile.data[x + 1][y] == ignoreMove[0])
         right = false;
     
     let possible = [];
@@ -119,23 +148,6 @@ function getPossibleMoves(x, y) {
     // normal move
     if(possible.length > 0)
         return possible;
-    // backtrack
-    // DOES NOT WORK
-    // maybe move to iteration loop
-    else if(path.length < (tile.data.length * tile.data[0].length)) {
-        console.log("backtracking");
-        // remove last movement of path
-        path.pop();
-
-        // make removed part of path availible as a move
-        tile.data[pos.x][pos.y] = 1;
-
-        // update position
-        pos.x = path[path.length - 1][0];
-        pos.y = path[path.length - 1][1];
-
-        getPossibleMoves(pos.x, pos.y);
-    }
     // entire board is filled
     else
         return false;
@@ -155,46 +167,34 @@ function renderBackground() {
     }
 }
 
-function renderPath() {
-    r.clearRect(0, 0, w, h);
+function renderPath() {    
+    if(mazeMode) {
+        r.strokeStyle = "#3F51B5";
+        r.lineWidth = 5;
+        let offset = tile.size / 4;
 
-    renderBackground();
-
-    r.strokeStyle = "#3F51B5";
-    r.lineWidth = 5;
-    let offset = tile.size / 4;
-
-    /*if(firstTime) {
-        r.fillStyle = "#2ECC71";
-
-        r.fillRect(
+        r.moveTo(
+            // x
+            (path[[path.length - 2]][0] * tile.size) + 
+            offset, 
+            // y
+            (path[path.length - 2][1] * tile.size) + 
+            offset);
+        
+        r.lineTo(
             // x
             (path[[path.length - 1]][0] * tile.size) + 
-            0, 
+            offset, 
             // y
             (path[path.length - 1][1] * tile.size) + 
-            0, 
-            // width
-            tile.size / 2, 
-            //height
-            tile.size / 2
-            )
-    }*/
+            offset);
+        
+        r.stroke();
+    }
+    else {
+        r.clearRect(0, 0, w, h);
 
-    r.moveTo(
-        // x
-        (path[[path.length - 2]][0] * tile.size) + 
-        offset, 
-        // y
-        (path[path.length - 2][1] * tile.size) + 
-        offset);
-    
-    r.lineTo(
-        // x
-        (path[[path.length - 1]][0] * tile.size) + 
-        offset, 
-        // y
-        (path[path.length - 1][1] * tile.size) + 
-        offset);
-    r.stroke();
+        if(showBackground)
+            renderBackground();
+    }
 }
